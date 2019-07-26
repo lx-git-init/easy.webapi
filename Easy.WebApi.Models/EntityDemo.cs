780d15c8549ac106891ad7bf8e7efc3c6b1aba86
lixiang <li.xiang@synyi.com>
2019-07-26 17:11:20 +0800

    将miniprofiler集成到swagger上

--- Easy.WebApi.Models/EntityDemo.cs
+++ Easy.WebApi.Models/EntityDemo.cs
@@ -2,12 +2,21 @@
 
 namespace Easy.WebApi.Models
 {
+    /// <summary>
+    ///
+    /// </summary>
     [Table("demo", Schema = "dbo")]
     public class EntityDemo
     {
+        /// <summary>
+        ///
+        /// </summary>
         [Column("id")]
         public int Id { get; set; }
 
+        /// <summary>
+        ///
+        /// </summary>
         [Column("name")]
         public string Name { get; set; }
     }
--- Easy.WebApi.Services/Startup.cs
+++ Easy.WebApi.Services/Startup.cs
@@ -7,25 +7,61 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Profiling.Storage;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Easy.WebApi.Services
 {
+    /// <summary>
+    ///
+    /// </summary>
     public class Startup
     {
         private readonly IConfiguration _configuration;
 
         private readonly IHostingEnvironment _env;
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="env"></param>
         public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             _configuration = configuration;
             _env = env;
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddMvc(opts =>
+            // 使用MiniProfiler插件分析EF http://localhost:5000/profiler/results-index
+            services.AddMiniProfiler(opts =>
+                {
+                    opts.RouteBasePath = "/profiler";
+                    ((MemoryCacheStorage)opts.Storage).CacheDuration = TimeSpan.FromSeconds(1);
+                })
+                .AddEntityFramework();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Easy WebApi"
+                });
+                var xmlMiniProfiler = Path.Combine(AppContext.BaseDirectory, "MiniProfiler.xml");
+                options.IncludeXmlComments(xmlMiniProfiler);
+                options.IgnoreObsoleteActions();
+            });
+
+            services.AddMvc(opts =>
                 {
                     opts.ModelMetadataDetailsProviders.Add(new RequiredBindingMetadataProvider());
                     opts.Filters.Add(new ActionResultFilter(_env));
@@ -34,17 +70,15 @@ namespace Easy.WebApi.Services
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<MyDbContext>(opts => opts.UseSqlServer(_configuration["ConnectionStrings:default"]));
-
-            // 使用MiniProfiler插件分析EF
-            services
-                .AddMiniProfiler(opts => opts.RouteBasePath = "/profiler")
-                .AddEntityFramework();
-
             services.AddPrivateConfig();
-
             services.AddLocalization();
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -57,8 +91,14 @@ namespace Easy.WebApi.Services
             }
 
             app.UseHttpsRedirection();
-
             app.UseMiniProfiler();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "swagger";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Easy WebApi V1");
+                c.IndexStream = () => GetType().GetTypeInfo().Assembly.GetManifestResourceStream("Easy.WebApi.Services.index.html");
+            });
             app.UseMvc();
         }
     }
